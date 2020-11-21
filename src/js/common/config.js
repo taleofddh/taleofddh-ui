66ec11ca663fe7ae5aca0411e212f33bd91253dc
devadyuti <devadyuti@yahoo.com>
2020-11-21 18:42:39 +0000

    Issue 1:Build Taleofddh User Interface - Setup

---
+++ src/js/common/config.js
@@ -0,0 +1,16 @@
+const config = {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "media.taleofddh.com",
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://api.taleofddh.com",
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_9faevWd35",
+        APP_CLIENT_ID: "5642rk8biiciamun2pppl6hfib",
+        IDENTITY_POOL_ID: "eu-west-1:34adc005-4841-467b-861f-da971f6eaa5c"
+    }
+};
