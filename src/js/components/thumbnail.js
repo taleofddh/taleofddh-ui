bce7206e2ed95a7c580728a0af22db856ba82e41
devadyuti <devadyuti@yahoo.com>
2020-11-25 16:13:01 +0000

    Issue 11:Tale of DDH - Sign-in Functionality

--- src/js/components/thumbnail.js
+++ src/js/components/thumbnail.js
@@ -3,14 +3,37 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import { MEDIA_HOST} from "../common/constants";
 import {useApi, useGet, useMediaQuery} from "../common/hook";
+import Gallery from 'react-grid-gallery';
 import '../../scss/components/thumbnail.scss';
 
 function Thumbnail(props) {
-
     const match = useMediaQuery('(max-width: 820px)');
+    let images = {};
+    images = props.pictures.map((item, index) => {
+        return {...images,
+            src : MEDIA_HOST + '/images/thumbnail/' + item.titlePhoto.path + '/' + item.titlePhoto.name + '.jpg',
+            thumbnail : MEDIA_HOST + '/images/thumbnail/' + item.titlePhoto.path + '/' + item.titlePhoto.name + '.jpg',
+            thumbnailWidth: (item.titlePhoto.maxWidth) / 6,
+            thumbnailHeight: (item.titlePhoto.maxHeight) / 6,
+            thumbnailCaption: item.name,
+            alt: item.name
+        };
+    })
+
+    const handleAlbumClick = async (index) => {
+        alert(images[index].caption);
+    }
 
     return (
-        <ul className="thumbnailgroup">
+        <div className="thumbnailset">
+            <Gallery images={images}
+                     rowHeight="180"
+                     enableLightbox={false}
+                     enableImageSelection={false}
+                     showLightboxThumbnails={true}
+                     onClickThumbnail={handleAlbumClick} />
+        </div>
+/*        <ul className="thumbnailgroup">
             {props.pictures.map((item, index) => (
                 <li className="thumbnailitem">
                     <div className="thumbnail" style ={{backgroundImage: 'url("' + MEDIA_HOST + '/images/thumbnail/' + item.titlePhoto.path + '/' + item.titlePhoto.name + '.jpg")', backgroundPositionX: 'center',
@@ -23,7 +46,7 @@ function Thumbnail(props) {
                 </li>
             ))
             }
-        </ul>
+        </ul>*/
     )
 
 }
