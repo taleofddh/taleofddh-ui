dda1f92087c0dd5d04e9db683e865c09a8447c78
devadyuti <devadyuti@yahoo.com>
2020-12-11 01:15:00 +0000

    Issue 6:Build of Blog Pages

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ import ContactUs from "./pages/contactus";
 import Acknowledgement from "./pages/acknowledgement";
 import Blog from "./pages/blog";
 import Article from "./pages/article";
-import MarkdownBlog from "./pages/markedownblog";
+import MarkdownBlog from "./pages/markdownblog";
 import Gallery from "./pages/gallery";
 import Photo from "./pages/photo";
 import TermsAndConditions from "./pages/termsandconditions";
