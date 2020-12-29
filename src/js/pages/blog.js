549533944cc3056a7786c0385414723f5f9e51ed
devadyuti <devadyuti@yahoo.com>
2020-12-29 22:23:24 +0000

    Issue 6:Build of Blog Page

--- src/js/pages/blog.js
+++ src/js/pages/blog.js
@@ -1,46 +1,32 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useHistory} from "react-router-dom";
-import {Auth} from "aws-amplify";
-import {useApi, useFetch, useMediaQuery} from '../common/hook'
-import {getSessionCookie, useSessionContext} from "../common/session";
-import {onError} from "../common/error";
+import {MEDIA_HOST, MONTH_NAMES} from "../common/constants";
+import {postAuditEntry} from "../common/common";
+import {useApi, useGet, useMediaQuery} from '../common/hook'
+import {getSessionCookie} from "../common/session";
 import MetaTag from "../components/metatag";
 import Title from "../components/title";
 import Loader from "../components/loader";
-import Album from "../components/album";
-import '../../scss/pages/gallery.scss';
-import {postAuditEntry} from "../common/common";
+import '../../scss/pages/blog.scss';
+import Icon from "../common/icon";
 
 const pagetitle = 'Blog'
 const source = 'blog';
 
 function Blog(props) {
     const history = useHistory();
-    const { isAuthenticated, userHasAuthenticated } = useSessionContext();
-    const [isAuthenticating, setIsAuthenticating] = useState(true);
     const [api, index] = useApi(window.location.hostname, window.location.protocol, 'api');
     const ddhomeCountry = getSessionCookie('ddhomeCountry');
-    const user = getSessionCookie('user');
-    let restrictedFlag = false;
-    if(user.roleList !== undefined) {
-        if(user.roleList.name === 'Friend' || user.roleList.name === 'Family') {
-            restrictedFlag = true;
-        }
-    }
-    let albumUri = '/gallery/albumList';
-    if(restrictedFlag) {
-        albumUri = albumUri + '/' + restrictedFlag;
-    }
-    const [data, loading] = useFetch(
-        api + albumUri, 'albums'
+    const [data, loading] = useGet(
+        'findBlogList',
+        '/blogList'
     );
 
     const matches = useMediaQuery('(max-width: 820px)');
 
     useEffect(() => {
-        onLoad();
         postAuditEntry(
             {
                 date: new Date(),
@@ -53,55 +39,51 @@ function Blog(props) {
         );
     }, [])
 
-    async function onLoad() {
-        try {
-            await Auth.currentSession();
-            userHasAuthenticated(true);
-        }
-        catch(e) {
-            if (e !== 'No current user') {
-                onError(e);
-            }
-        }
-        setIsAuthenticating(false);
-    }
-
     const handleClick = (clickEvent, object) => {
-        if(isAuthenticated) {
-            //alert(object.photo.caption);
-            let album = object.photo;
-            props.history.push('/photo', {
-                api: api,
-                index: index,
-                album: album
-            });
-        } else {
-            alert("Not Authorised. Please login");
-        }
-
+        clickEvent.preventDefault();
+        let blogName = object.name;
+        history.push('/blog/article', {
+            api: api,
+            index: index,
+            blog: {
+                name: blogName
+            }
+        });
     }
 
     return (
-        !isAuthenticating && (
-            <>
-                <MetaTag page={source} index={index} url={window.location.protocol + '//'  + window.location.hostname} />
-                <div className="boxouter">
-                    <div className="container">
-                        <div className="galleryframe">
-                            <Title message={pagetitle} />
-                            {loading ? (
-                                <Loader loading={loading} />
-                            ) : (
-                                <Album pictures={data} onClick={handleClick}/>
-                            )}
-                        </div>
+        <>
+            <MetaTag page={source} index={index} url={window.location.protocol + '//'  + window.location.hostname} />
+            <div className="boxouter">
+                <div className="container">
+                    <div className="blogframe">
+                        <Title message={pagetitle} />
+                        {loading ? (
+                            <Loader loading={loading} />
+                        ) : (
+                            <ul className="bloggroup">
+                                {data.map((item, index) => (
+                                    <li key={index} className="blogitem" onClick={(e) => handleClick(e, item)}>
+                                        <div style={{float: 'left'}}>
+                                            <div className="blogpiccontrol">
+                                                <img src={MEDIA_HOST + '/images/mobile/' + item.titlePhoto} />
+                                            </div>
+                                            <div className="blogsummary">
+                                                <p className="blogtitle">{item.header}</p>
+                                                <p className="blogtext">{'by ' + item.author + ' on ' + new Date(item.endDate).getDate() + " " + MONTH_NAMES[new Date(item.endDate).getMonth()] + ", " + new Date(item.endDate).getFullYear()}</p>
+                                                <p className="blogtext">{item.title}</p>
+                                                <p className="blogtext"><Icon name="view" width="1rem" height="1rem"/>&nbsp;&nbsp;{item.viewCount}&nbsp;&nbsp;<Icon name="share" width="1rem" height="1rem"/></p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
                 </div>
-            </>
-        )
+            </div>
+        </>
     )
 }
-
-
 
 export default Blog;
