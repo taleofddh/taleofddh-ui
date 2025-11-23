3dbdb11575ff70537b216ac1de4db1cf73355c98
devadyuti <devadyuti@yahoo.com>
2025-11-23 22:19:57 +0000

    Refactor Javascript File names as xxx-yyy-zzz.js #32

--- components/photo-album.js
+++ components/photo-album.js
@@ -7,26 +7,26 @@ import ReactPlayer from "react-player";
 
 function PhotoAlbum({ source, path, type, album, photos, videos }) {
     return (
-        <div className="albumdetailsframe">
+        <div className="photoalbumframe">
             <Title message={album.name} />
-            <ul className="albumdetailsgroup">
-                <li className="albumdetailsimagecontainer">
-                    <div className="albumdetailsimage">
+            <ul className="photoalbumgroup">
+                <li className="photoalbumimagecontainer">
+                    <div className="photoalbumimage">
                         <img src={album.signedUrl} alt={album.titlePhoto} />
                     </div>
                 </li>
-                <li className="albumdetailstextcontainer">
-                    <div className="albumdetailsdescription">
+                <li className="photoalbumtextcontainer">
+                    <div className="photoalbumdescription">
                         {ReactHtmlParser(marked(album.description.toString()))}
                     </div>
                 </li>
-                <li className="albumdetailsgallerycontainer">
-                    <div className="albumdetailsdescription">
+                <li className="photoalbumgallerycontainer">
+                    <div className="photoalbumdescription">
                         <Carousel source={source} path={path} photos={photos} />
                     </div>
                 </li>
-                <li className="albumdetailsgallerycontainer">
-                    <div className="albumdetailsdescription">
+                <li className="photoalbumgallerycontainer">
+                    <div className="photoalbumdescription">
                         {videos.map((item, index) => (
                             <ReactPlayer key={index} src={item}  width='100%' height='100%' controls={true}/>
                         ))}
